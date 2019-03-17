bc79359b30dba18fb4f3c51b3b6570b974662940
matianqi <mtq1997@126.com>
2019-03-17 10:15:49 +0800

    add ssl key

---
+++ app.js
@@ -0,0 +1,47 @@
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import typeDefs from './graphql/schema'
+import resolvers from './graphql/resolvers'
+import fs from 'fs'
+import https from 'https'
+import http from 'http'
+
+const configurations = {
+  // Note: You may need sudo to run on port 443
+  production: { ssl: true, port: 443, hostname: 'example.com' },
+  development: { ssl: false, port: 4000, hostname: 'localhost' }
+}
+
+const environment = process.env.NODE_ENV || 'production'
+const config = configurations[environment]
+
+const apollo = new ApolloServer({ typeDefs, resolvers })
+
+const app = express()
+apollo.applyMiddleware({ app })
+
+// Create the HTTPS or HTTP server, per configuration
+var server
+if (config.ssl) {
+  // Assumes certificates are in .ssl folder from package root. Make sure the files
+  // are secured.
+  server = https.createServer(
+    {
+      key: fs.readFileSync(`./ssl/${environment}/server.key`),
+      cert: fs.readFileSync(`./ssl/${environment}/server.crt`)
+    },
+    app
+  )
+} else {
+  server = http.createServer(app)
+}
+
+// Add subscription support
+apollo.installSubscriptionHandlers(server)
+
+server.listen({ port: config.port }, () =>
+  console.log(
+    '🚀 Server ready at',
+    `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${apollo.graphqlPath}`
+  )
+)
