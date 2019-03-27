9a95fac8c83d4dcd884d9b205d48e9380f380196
matianqi <mtq1997@126.com>
2019-03-27 18:27:58 +0800

    add file upload

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import fs from 'fs'
 import https from 'https'
 import http from 'http'
 import cors from'cors'
+import multer from 'multer'
 
 import session from './session'
 
@@ -60,13 +61,47 @@ const apollo = new ApolloServer({
 
 const app = express()
 
+// 跨域配置信息
 const corsOptions = {
   origin: 'https://localhost:3000',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true
 }
 
+// 支持跨域
 app.use(cors(corsOptions));
+
+var createFolder = function(folder){
+  try{
+      fs.accessSync(folder); 
+  }catch(e){
+      fs.mkdirSync(folder);
+  }  
+};
+
+var uploadFolder = './upload/';
+
+createFolder(uploadFolder);
+
+// 通过 filename 属性定制
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, uploadFolder);    // 保存的路径，备注：需要自己创建
+  },
+  filename: function (req, file, cb) {
+      // 将保存文件名设置为 字段名 + 时间戳，比如 logo-1478521468943
+      cb(null, file.fieldname + '-' + Date.now());  
+  }
+});
+
+// 通过 storage 选项来对 上传行为 进行定制化
+var upload = multer({ storage: storage })
+
+// 单图上传
+app.post('/upload', upload.single('logo'), function(req, res, next){
+  var file = req.file;
+  res.send({ret_code: '0'});
+});
 
 apollo.applyMiddleware({ app })
 
