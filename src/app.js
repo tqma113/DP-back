b9e7b5f1874d74df11a608680d801d033f46a30f
Ma Tianqi <mtq1997@126.com>
2019-12-06 21:45:14 +0800

    add babel

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import typeDefs from './graphql/schema'
